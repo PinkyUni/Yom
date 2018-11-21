baee616582216153d3f69d4a961d1ea4f48005b2
PinkyUni <anya.beliatskaya.751001@gmail.com>
2018-11-21 22:19:45 +0300

    Added list of cakes with clickListeners

--- app/src/main/java/fragments/AllRecipesFragment.java
+++ app/src/main/java/fragments/AllRecipesFragment.java
@@ -1,17 +1,21 @@
 package fragments;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.yom.MainActivity;
 import com.yom.MainViewModel;
 import com.yom.MyRecyclerViewAdapter;
 import com.yom.R;
@@ -19,12 +23,16 @@ import com.yom.Recipe;
 
 import java.util.ArrayList;
 
+import static com.yom.MainActivity.typefaceBold;
+
 public class AllRecipesFragment extends Fragment {
 
     public FragmentTransaction fragmentTransaction;
     public MainViewModel mainViewModel;
     private MyRecyclerViewAdapter adapter;
     private ArrayList<Recipe> recipeBook;
+    private ArrayList<Recipe> cakeRecipes;
+    private ArrayList<Recipe> brownieRecipes;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -45,13 +53,29 @@ public class AllRecipesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.all_recipes_fragment, null);
-        RecyclerView recyclerView = view.findViewById(R.id.Recipes);
+        final RecyclerView recyclerView = view.findViewById(R.id.Recipes);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         adapter = new MyRecyclerViewAdapter(getActivity(), recipeBook);
         adapter.setClickListener(new MyRecyclerViewAdapter.ItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                Toast.makeText(getActivity(), "You clicked " + adapter.getItemName(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getActivity(), "You clicked " + adapter.getItemName(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+                switch (position) {
+                    case 0:
+                        cakeRecipes = new ArrayList<>();
+                        cakeRecipes.add(new Recipe(getResources().getString(R.string.cake_napoleon), getResources().getDrawable(R.drawable.napoleon)));
+                        cakeRecipes.add(new Recipe(getResources().getString(R.string.cake_graph_ruins), getResources().getDrawable(R.drawable.graph_ruins)));
+                        adapter = new MyRecyclerViewAdapter(getActivity(), cakeRecipes);
+                        adapter.setClickListener(new MyRecyclerViewAdapter.ItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Toast.makeText(getActivity(), "You clicked " + adapter.getItemName(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                        recyclerView.setAdapter(adapter);
+                        break;
+                }
             }
         });
         recyclerView.setAdapter(adapter);
